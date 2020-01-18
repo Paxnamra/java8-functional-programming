f94483b58f0648746dd9f757a15b2d1d75fee5ce
Paxnamra <bialyfeniks4@gmail.com>
2020-01-18 19:38:00 +0100

    2.5 getting distinct strings - UnaryOperator example

--- src/main/java/_2_5_getting_distinct_strings/Step1.java
+++ src/main/java/_2_5_getting_distinct_strings/Step1.java
@@ -1,14 +1,28 @@
 package _2_5_getting_distinct_strings;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.function.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Step1 {
     public static void main(String[] args) {
-        List<String> listOfStrings = new ArrayList<>(Arrays.asList("java", "scala", "clojure", "clojure", "java", "scala"));
 
-        System.out.println(listOfStrings.stream().distinct().collect(Collectors.toList()));
+//        Write a lambda expression that accepts a list of strings and returns new list of distinct strings (without repeating).
+//        The order of elements in the result list may be any. If the input list doesn't contain any strings then the result list should be empty.
+//        Hints: it is possible to use sets, maps, lists and so on for helping.
+//        Sample Input 1: java scala java clojure clojure || Sample Output 1: clojure java scala
+//        Sample Input 2: the three the three the three an an a || Sample Output 2: a an the three
+
+        List<String> listOfStrings = new ArrayList<>(Arrays.asList("java", "scala", "clojure", "clojure", "java", "scala"));
+        System.out.println("Result with stream: " + listOfStrings.stream().distinct().collect(Collectors.toList()));
+
+        //way with UnaryOperator
+        UnaryOperator<List<String>> operator = x -> {
+            return x.stream().distinct().collect(Collectors.toList());
+        };
+
+        UnaryOperator<List<String>> op = x -> x.stream().distinct().collect(Collectors.toList());
+        System.out.println("Result with lambda: " + operator.apply(listOfStrings));
     }
 }
