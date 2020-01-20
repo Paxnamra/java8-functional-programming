4f633c61eb3f2f18231f75114773ecc26a25f6fc
Paxnamra <bialyfeniks4@gmail.com>
2020-01-20 22:39:23 +0100

    experimenting more with solution method

--- src/main/java/_2_19_numbers_filtering/Step1.java
+++ src/main/java/_2_19_numbers_filtering/Step1.java
@@ -12,16 +12,30 @@ import java.util.stream.IntStream;
 public class Step1 {
     public static void main(String[] args) {
 
+        //several streams because they close after they end and are not reusable
         IntStream example1 = IntStream.of(30, 90, 40, 21, 120);
         IntStream example2 = IntStream.of(60, 75, 13, 2);
+        IntStream example3 = IntStream.of(30, 90, 40, 21, 120);
+        IntStream example4 = IntStream.of(60, 75, 13, 2);
 
-        List<Integer> passedStream = createFilteringStream(example1, example2).boxed().collect(Collectors.toList());
+        List<Integer> passedStream = createFilteringStream1(example1, example2).boxed().collect(Collectors.toList());
         System.out.println(passedStream.stream().sorted().skip(2).collect(Collectors.toList()));
+
+        System.out.println(createFilteringStream2(example3, example4).boxed().collect(Collectors.toList()));
     }
 
-    public static IntStream createFilteringStream(IntStream evenStream, IntStream oddStream) {
+    public static IntStream createFilteringStream1(IntStream evenStream, IntStream oddStream) {
         return IntStream.concat(oddStream, evenStream)
                 .filter(i -> i % 3 == 0 &&
                         i % 5 == 0);
+    }
+
+    public static IntStream createFilteringStream2(IntStream evenStream, IntStream oddStream) {
+        return IntStream.concat(oddStream, evenStream)
+                .filter(i -> i % 3 == 0 && i % 5 == 0)
+                .boxed()
+                .sorted()
+                .skip(2)
+                .mapToInt(p -> p);
     }
 }
