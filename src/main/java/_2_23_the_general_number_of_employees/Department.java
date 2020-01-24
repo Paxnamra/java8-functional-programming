f96d812a90b9e9396039bb47d664a8ff8d652144
Paxnamra <bialyfeniks4@gmail.com>
2020-01-24 10:42:39 +0100

    replaced boilerplate in 2.23 with lombok

--- src/main/java/_2_23_the_general_number_of_employees/Department.java
+++ src/main/java/_2_23_the_general_number_of_employees/Department.java
@@ -1,36 +1,16 @@
 package _2_23_the_general_number_of_employees;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
 import java.util.List;
 
+@Getter
+@AllArgsConstructor
+@Data
 public class Department {
     private String name;
     private String code;
     private List<Employee> employee;
-
-    public Department(String name, String code, List<Employee> employee) {
-        this.name = name;
-        this.code = code;
-        this.employee = employee;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public List<Employee> getEmployee() {
-        return employee;
-    }
-
-    @Override
-    public String toString() {
-        return "Department{" +
-                "name='" + name + '\'' +
-                ", code='" + code + '\'' +
-                ", employee=" + employee +
-                '}';
-    }
 }
--- src/main/java/_2_23_the_general_number_of_employees/Employee.java
+++ src/main/java/_2_23_the_general_number_of_employees/Employee.java
@@ -1,27 +1,13 @@
 package _2_23_the_general_number_of_employees;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Data
 public class Employee {
     private String name;
     private long salary;
-
-    public Employee(String name, long salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public long getSalary() {
-        return salary;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "name='" + name + '\'' +
-                ", salary=" + salary +
-                '}';
-    }
 }
