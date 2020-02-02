b8445208811b96c980dd35d9e0bae385cc5dca05
Paxnamra <bialyfeniks4@gmail.com>
2020-02-02 13:52:34 +0100

    ascending and descending sorting

---
+++ src/main/java/jetBrains_academy/AscDescSort.java
@@ -0,0 +1,38 @@
+package jetBrains_academy;
+
+import java.util.*;
+import java.util.function.*;
+
+public class AscDescSort {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean isAscending = scanner.nextLine().equals("ascending");
+        int[] array = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        BiFunction<Integer, Integer, Integer> comparator = (i1, i2) -> {
+            if (isAscending) {
+                return (i1 <= i2) ? i1 : i2;
+            } else {
+                return (i1 >= i2) ? i1 : i2;
+            }
+        };
+
+        sort(array, comparator);
+        Arrays.stream(array).forEach(e -> System.out.print(e + " "));
+    }
+
+    public static void sort(int[] array, BiFunction<Integer, Integer, Integer> comparator) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (comparator.apply(array[j], array[j + 1]) == array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
