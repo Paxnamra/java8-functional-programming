0f9e72acccb1036f7304a9d3c032fc57e8454a9c
Paxnamra <bialyfeniks4@gmail.com>
2020-02-10 23:53:57 +0100

    2.9 refactor of objects and methods namings

--- src/main/java/_2_9_behaviour_parametrization_with_lambda/Step1.java
+++ src/main/java/_2_9_behaviour_parametrization_with_lambda/Step1.java
@@ -4,24 +4,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-//Write a code for filtering the accounts list in two ways:
-//get list with all non-empty accounts (balance > 0) and save it to the variable nonEmptyAccounts
-//get all non-locked accounts with too much money (balance >= 100 000 000) and save it to the variable accountsWithTooMuchMoney
+/**
+ * Write a code for filtering the accounts list in two ways:
+ * get list with all non-empty accounts (balance > 0) and save it to the variable nonEmptyAccounts
+ * get all non-locked accounts with too much money (balance >= 100 000 000) and save it to the variable accountsWithTooMuchMoney
+ */
 
 public class Step1 {
     public static void main(String[] args) {
 
-        List<Account> inDat = new ArrayList<>(Arrays.asList(
+        List<Account> accounts = new ArrayList<>(Arrays.asList(
                 new Account("1234567", 100000000L, false),
                 new Account("4433221", 100000000L, true),
                 new Account("9876521", 2000L, false),
                 new Account("5433210", 2300L, false)
         ));
 
-        Account a1 = new Account();
+        Account account = new Account();
 
-        List<Account> nonEmptyAccounts = a1.filter(inDat, a1.nonEmptyAccounts());
-        List<Account> accountsWithTooMuchMoney = a1.filter(inDat, a1.accountsWithTooMuchMoney());
+        List<Account> nonEmptyAccounts = account.filter(accounts, account.nonEmptyAccounts());
+        List<Account> accountsWithTooMuchMoney = account.filter(accounts, account.accountsWithTooMuchMoney());
 
         System.out.println(nonEmptyAccounts);
         System.out.println(accountsWithTooMuchMoney);
