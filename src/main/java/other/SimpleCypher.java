408b700e919860d97570924cedc65d104f551dcb
Paxnamra <bialyfeniks4@gmail.com>
2020-02-15 23:43:59 +0100

    simple cypher refactor using streams - attempt

--- src/main/java/other/SimpleCypher.java
+++ src/main/java/other/SimpleCypher.java
@@ -3,7 +3,7 @@ package other;
 public class SimpleCypher {
     public static void main(String[] args) {
 
-        //iteration over chars
+        //iterative chars
         String text = "we found a treasure!";
         for (int i = 0; i < text.length(); i++) {
             if (text.charAt(i) > 96 && text.charAt(i) < 124) {
@@ -12,5 +12,18 @@ public class SimpleCypher {
                 System.out.print(text.substring(i, i + 1));
             }
         }
+
+        System.out.println();
+
+        String s = text.chars().filter(o -> o != ' ').map(e -> 97 - e + 122).collect(StringBuilder::new,
+                (sb, s1) -> sb.append(s1).append(" "),
+                (sb1, sb2) -> sb1.append(sb2.toString())).toString();
+
+        String f = text.chars().map(e -> 97 - e + 122).collect(StringBuilder::new,
+                (sb, s1) -> sb.append(s1).append(" "),
+                (sb1, sb2) -> sb1.append(sb2.toString())).toString();
+
+        System.out.println(s);
+        System.out.println(f);
     }
 }
