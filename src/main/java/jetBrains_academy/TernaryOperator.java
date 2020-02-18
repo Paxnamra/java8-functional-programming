f9fd991eb0fce0a374fd44395f2de8b7f8aa2736
Paxnamra <bialyfeniks4@gmail.com>
2020-02-18 23:25:31 +0100

    description of a task

--- src/main/java/jetBrains_academy/TernaryOperator.java
+++ src/main/java/jetBrains_academy/TernaryOperator.java
@@ -3,6 +3,13 @@ package jetBrains_academy;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
+/**
+ * Let's combine function to build more complex functions using standard functional interfaces.
+ * Given the predicate condition and two functions ifTrue and ifFalse.
+ * Implement a method ternaryOperator which builds a new function returning the result of invoking ifTrue,
+ * if the predicate is true and otherwise - the result of invoking ifFalse.
+ */
+
 public class TernaryOperator {
 
     public static <T, U> Function<T, U> ternaryOperator(
