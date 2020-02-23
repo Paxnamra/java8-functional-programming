721f1e88fed233eea3e7ba25e8f4efb06f81a553
Paxnamra <bialyfeniks4@gmail.com>
2020-02-23 14:44:24 +0100

    from string to char[] to ints modify and back to string with stream

--- src/main/java/other/SimpleCypher.java
+++ src/main/java/other/SimpleCypher.java
@@ -1,5 +1,9 @@
 package other;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class SimpleCypher {
     public static void main(String[] args) {
 
@@ -15,15 +19,20 @@ public class SimpleCypher {
 
         System.out.println();
 
-        String s = text.chars().filter(o -> o != ' ').map(e -> 97 - e + 122).collect(StringBuilder::new,
-                (sb, s1) -> sb.append(s1).append(" "),
+        String s = text.chars().filter(o -> o != ' ').map(e -> 97 - e + 122).mapToObj(r -> (char) r).collect(StringBuilder::new,
+                (sb, s1) -> sb.append(s1),
                 (sb1, sb2) -> sb1.append(sb2.toString())).toString();
 
         String f = text.chars().map(e -> 97 - e + 122).collect(StringBuilder::new,
                 (sb, s1) -> sb.append(s1).append(" "),
                 (sb1, sb2) -> sb1.append(sb2.toString())).toString();
 
+        List<Character> sa = Stream.of(text).flatMap(e -> text.chars()
+        .map(w -> 97 - w + 122).mapToObj(r -> (char) r)).collect(Collectors.toList());
+
         System.out.println(s);
         System.out.println(f);
+        System.out.println(sa);
+
     }
 }
